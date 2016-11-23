dec390ca14137c50efff8658e8436598b0e12287
LuNoX <maillunox@gmail.com>
2016-11-23 22:03:50 +0100

    Cleaned up old code and made stuff pretty again

--- cogs/smashy.py
+++ cogs/smashy.py
@@ -21,6 +21,7 @@ class Smashy:
         if ctx.invoked_subcommand is None:
             await self.bot.say('Invalid subcommand passed: {0.subcommand_passed}'.format(ctx))
 
+    # TODO make remove commands use the argslist correctly
     @commands.group(pass_context=True, no_pm=True)
     async def remove(self, ctx):
         if ctx.invoked_subcommand is None:
@@ -40,8 +41,7 @@ class Smashy:
 
     @add.command(name='tournament')
     @checks.admin_or_permissions()
-    async def add_tournament(self, *, tournament_names: str):
-        tournament_names = tournament_names.split(" ")
+    async def add_tournament(self, *tournament_names: str):
         for tournament_name in tournament_names:
             await self.add_specific_tournament(tournament_name)
         await self.bot.say('\N{OK HAND SIGN}')
@@ -53,14 +53,9 @@ class Smashy:
         tournament_names.append(tournament_name)
         await self.config.put('tournament_names', tournament_names)
 
-    # TODO remove remove_specific funcs
     @remove.command(name='tournament')
     @checks.admin_or_permissions()
-    async def remove_tournament(self, *, tournament_name: str):
-        self.remove_specific_tournament(tournament_name)
-        await self.bot.say('\N{OK HAND SIGN}')
-
-    async def remove_specific_tournament(self, tournament_name):
+    async def remove_tournament(self, *tournament_name: str):
         tournament_names = self.config.get('tournament_names', [])
         if tournament_name in tournament_names:
             try:
@@ -69,14 +64,21 @@ class Smashy:
                 pass
 
         await self.config.put('tournament_names', tournament_names)
+        await self.bot.say('\N{OK HAND SIGN}')
 
     @get.group(name='events', invoke_without_command=True)
     @checks.admin_or_permissions()
-    async def get_events(self, *, tournament_names: str = None):
-        if tournament_names is None:
-            tournament_names = self.config.get('tournament_names', [])
-        else:
-            tournament_names = tournament_names.split(" ")
+    async def get_events(self):
+        tournament_names = self.config.get('tournament_names', [])
+        for tournament_name in tournament_names:
+            event_names = smash.tournament_show_events(tournament_name)
+            for event_name in event_names['events']:
+                await self.add_specific_event(event_name)
+        await self.bot.say('\N{OK HAND SIGN}')
+
+    @get_events.group(name='tournament')
+    @checks.admin_or_permissions()
+    async def get_events_tournament(self, *tournament_names: str):
         for tournament_name in tournament_names:
             event_names = smash.tournament_show_events(tournament_name)
             for event_name in event_names['events']:
@@ -85,8 +87,7 @@ class Smashy:
 
     @add.command(name='event')
     @checks.admin_or_permissions()
-    async def add_event(self, *, event_names: str):
-        event_names = event_names.split(" ")
+    async def add_event(self, *event_names: str):
         for event_name in event_names:
             await self.add_specific_event(event_name)
         await self.bot.say('\N{OK HAND SIGN}')
@@ -100,11 +101,7 @@ class Smashy:
 
     @remove.command(name='event')
     @checks.admin_or_permissions()
-    async def remove_event(self, *, event_name: str):
-        self.remove_specific_event(event_name)
-        await self.bot.say('\N{OK HAND SIGN}')
-
-    async def remove_specific_event(self, event_name):
+    async def remove_event(self, *event_name: str):
         event_names = self.config.get('event_names', [])
         if event_name in event_names:
             try:
@@ -112,11 +109,12 @@ class Smashy:
             except ValueError:
                 pass
         await self.config.put('event_names', event_names)
+        await self.bot.say('\N{OK HAND SIGN}')
 
     # TODO create the commands tournament and event for the get_brackets.group
     @get.group(name='brackets', invoke_without_command=True)
     @checks.admin_or_permissions()
-    async def get_brackets(self, *, tournament_names: str):
+    async def get_brackets(self, *tournament_names: str):
         if tournament_names is None:
             tournament_names = self.config.get('tournament_names', [])
         else:
@@ -131,8 +129,7 @@ class Smashy:
 
     @add.command(name='bracket')
     @checks.admin_or_permissions()
-    async def add_bracket(self, *, bracket_ids: str):
-        bracket_ids = bracket_ids.split(" ")
+    async def add_bracket(self, *bracket_ids: str):
         for bracket_id in bracket_ids:
             await self.add_bracket(bracket_id)
         await self.bot.say('\N{OK HAND SIGN}')
@@ -146,11 +143,7 @@ class Smashy:
 
     @remove.command(name='bracket')
     @checks.admin_or_permissions()
-    async def remove_bracket(self, *, bracket_id: str):
-        self.remove_specific_bracket(bracket_id)
-        await self.bot.say('\N{OK HAND SIGN}')
-
-    async def remove_specific_bracket(self, bracket_id):
+    async def remove_bracket(self, *bracket_id: str):
         bracket_ids = self.config.get('bracket_ids', [])
         if bracket_id in bracket_ids:
             try:
@@ -177,7 +170,7 @@ class Smashy:
 
     @get_sets.command('tournament')
     @checks.admin_or_permissions()
-    async def get_sets_tournament(self, *, tournament_names: str):
+    async def get_sets_tournament(self, *tournament_names: str):
         tournament_names = tournament_names.split(" ")
         for tournament_name in tournament_names:
             events = smash.tournament_show_events(tournament_name)
@@ -189,8 +182,7 @@ class Smashy:
 
     @get_sets.command('event')
     @checks.admin_or_permissions()
-    async def get_sets_event(self, *, event_names: str):
-        event_names = event_names.split(" ")
+    async def get_sets_event(self, *event_names: str):
         tournament_names = self.config.get('tournament_names', [])
         for tournament_name in tournament_names:
             for event_name in event_names:
@@ -201,8 +193,7 @@ class Smashy:
 
     @get_sets.command('bracket')
     @checks.admin_or_permissions()
-    async def get_sets_bracket(self, *, bracket_ids: str):
-        bracket_ids = bracket_ids.split(" ")
+    async def get_sets_bracket(self, *bracket_ids: str):
         for bracket_id in bracket_ids:
             sets = smash.bracket_show_sets(bracket_id)
             for specific_set in sets:
@@ -211,16 +202,14 @@ class Smashy:
 
     @add.command(name='set')
     @checks.admin_or_permissions()
-    async def add_set(self, *, set_ids: str):
-        set_ids = set_ids.split(" ")
+    async def add_set(self, *set_ids: str):
         for set_id in set_ids:
             await self.add_specific_set(set_id)
         await self.bot.say('\N{OK HAND SIGN}')
 
     @add.command(name='displayed_set')
     @checks.admin_or_permissions()
-    async def add_displayed_set(self, *, displayed_set_ids: str):
-        displayed_set_ids = displayed_set_ids.split(" ")
+    async def add_displayed_set(self, *displayed_set_ids: str):
         for displayed_set_id in displayed_set_ids:
             await self.add_specific_set(displayed_set_id)
             await self.add_specific_displayed_set(displayed_set_id)
@@ -242,27 +231,7 @@ class Smashy:
 
     @remove.command(name='displayed_set')
     @checks.admin_or_permissions()
-    async def remove_displayed_set(self, *, displayed_set_id: str):
-        await self.remove_specific_displayed_set(displayed_set_id)
-        await self.bot.say('\N{OK HAND SIGN}')
-
-    @remove.command(name='set')
-    @checks.admin_or_permissions()
-    async def remove_set(self, *, set_id: str):
-        await self.remove_specific_set(set_id)
-        await self.remove_specific_displayed_set(set_id)
-        await self.bot.say('\N{OK HAND SIGN}')
-
-    async def remove_specific_set(self, set_id: str):
-        set_ids = self.config.get('set_ids', [])
-        if set_id in set_ids:
-            try:
-                set_ids.remove(set_id)
-            except ValueError:
-                pass
-        await self.config.put('set_ids', set_ids)
-
-    async def remove_specific_displayed_set(self, displayed_set_id: str):
+    async def remove_displayed_set(self, *displayed_set_id: str):
         displayed_set_ids = self.config.get('displayed_set_ids', [])
         if displayed_set_id in displayed_set_ids:
             try:
@@ -270,6 +239,19 @@ class Smashy:
             except ValueError:
                 pass
         await self.config.put('displayed_set_ids', displayed_set_ids)
+        await self.bot.say('\N{OK HAND SIGN}')
+
+    @remove.command(name='set', pass_context=True)
+    @checks.admin_or_permissions()
+    async def remove_set(self, ctx, *set_id: str):
+        set_ids = self.config.get('set_ids', [])
+        if set_id in set_ids:
+            try:
+                set_ids.remove(set_id)
+            except ValueError:
+                pass
+        await self.config.put('set_ids', set_ids)
+        await ctx.invoke(self.remove_displayed_set, *set_id)
 
     @remove.group(name='all', invoke_without_command=True)
     @checks.is_owner()
